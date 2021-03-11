8f47ee385f3f91484fe9a9496f14ce12e29384c0
Luca Schmider <lschmider>
2021-03-11 15:05:12 +0100

    upgrade to flutter 2

--- lib/pages/refuel/refuel-main.dart
+++ lib/pages/refuel/refuel-main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fueler/model/app-state.model.dart';
 import 'package:fueler/model/refueling.model.dart';
@@ -14,131 +16,88 @@ class _RefuelMainState extends State<RefuelMain> {
   FuelType _fuelType = FuelType.Gazoline5;
   double _amount = 0;
   double _totalPrice = 0;
-  TextEditingController _amountController;
-  TextEditingController _priceController;
 
   bool _isDisabled() =>
-      _fuelType == null ||
       _amount == null ||
       _amount <= 0 ||
       _totalPrice == null ||
       _totalPrice <= 0;
 
-  Widget _buildAmountField() {
-    return CupertinoTextField(
-      controller: _amountController,
-      prefix: const Icon(
-        CupertinoIcons.drop_fill,
-        color: CupertinoColors.lightBackgroundGray,
-        size: 28,
-      ),
-      suffix: Text("Liter"),
-      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
-      clearButtonMode: OverlayVisibilityMode.editing,
-      keyboardType: TextInputType.number,
-      autocorrect: false,
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            width: 0,
-            color: CupertinoColors.inactiveGray,
-          ),
-        ),
-      ),
-      placeholder: "Menge",
-      onChanged: (newAmount) {
-        setState(() => _amount = double.tryParse(newAmount));
-      },
-    );
-  }
-
-  Widget _buildTotalPriceField() {
-    return CupertinoTextField(
-      controller: _priceController,
-      prefix: const Icon(
-        CupertinoIcons.money_euro,
-        color: CupertinoColors.lightBackgroundGray,
-        size: 28,
-      ),
-      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
-      clearButtonMode: OverlayVisibilityMode.editing,
-      keyboardType: TextInputType.number,
-      autocorrect: false,
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            width: 0,
-            color: CupertinoColors.inactiveGray,
-          ),
-        ),
-      ),
-      placeholder: "Preis",
-      onChanged: (newPrice) {
-        setState(() {
-          return _totalPrice = double.tryParse(newPrice);
-        });
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Column(
-        children: <Widget>[
-          CupertinoNavigationBar(
-            middle: Text("Nachtanken"),
-            automaticallyImplyLeading: false,
-          ),
-          Column(
-            children: [
-              Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: _buildAmountField()),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: _buildTotalPriceField(),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: SizedBox(
-                  width: double.maxFinite,
-                  child: CupertinoSlidingSegmentedControl(
-                    groupValue: _fuelType,
-                    children: {
-                      FuelType.Gazoline5: Text("Super E5"),
-                      FuelType.Gazoline10: Text("Super E10"),
-                      FuelType.Diesel: Text("Diesel"),
-                    },
-                    onValueChanged: (fuelType) => setState(() {
-                      _fuelType = fuelType;
-                    }),
-                  ),
+      navigationBar: CupertinoNavigationBar(
+        automaticallyImplyLeading: false,
+        middle: Text("Nachtanken"),
+      ),
+      resizeToAvoidBottomInset: false,
+      child: SafeArea(
+        child: Container(
+          color: CupertinoColors.systemGroupedBackground,
+          child: Form(
+            child: Column(
+              children: [
+                CupertinoFormSection(
+                  header: Text("Allgemeine Informationen"),
+                  children: [
+                    CupertinoFormRow(
+                      prefix: Text("Kraftstoff"),
+                      child: CupertinoSlidingSegmentedControl(
+                        groupValue: _fuelType,
+                        children: {
+                          FuelType.Gazoline5: Text("Super E5"),
+                          FuelType.Gazoline10: Text("Super E10"),
+                          FuelType.Diesel: Text("Diesel"),
+                        },
+                        onValueChanged: (fuelType) => setState(() {
+                          _fuelType = fuelType;
+                        }),
+                      ),
+                    ),
+                    CupertinoTextFormFieldRow(
+                      prefix: Text("Menge"),
+                      keyboardType: TextInputType.numberWithOptions(
+                          decimal: true, signed: false),
+                      onChanged: (value) => setState(() {
+                        _amount = double.tryParse(value);
+                      }),
+                      textAlign: TextAlign.end,
+                    ),
+                    CupertinoTextFormFieldRow(
+                      prefix: Text("Preis"),
+                      keyboardType: TextInputType.numberWithOptions(
+                          decimal: true, signed: false),
+                      onChanged: (value) => setState(() {
+                        _totalPrice = double.tryParse(value);
+                      }),
+                      textAlign: TextAlign.end,
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  width: double.maxFinite,
-                  child: CupertinoButton.filled(
-                    child: Text("Tanken"),
-                    onPressed: _isDisabled()
-                        ? null
-                        : () async {
-                            Refueling newRefueling = Refueling.capture(
-                                _amount, _totalPrice, _fuelType);
-                            Provider.of<AppStateModel>(context, listen: false)
-                                .registerRefueling(newRefueling);
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: SizedBox(
+                    width: double.maxFinite,
+                    child: CupertinoButton.filled(
+                      child: Text("Tanken"),
+                      onPressed: _isDisabled()
+                          ? null
+                          : () async {
+                              var provider = Provider.of<AppStateModel>(context,
+                                  listen: false);
+                              var refueling = Refueling.capture(
+                                  _amount, _totalPrice, _fuelType);
 
-                            Navigator.pop(context);
-                          },
+                              await provider.registerRefueling(refueling);
+                              Navigator.pop(context);
+                            },
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
